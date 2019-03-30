419ba8ed638ab02a9642737c8c34e00904c21cbe
Ediel96 <h.amiltone.diel@gmail.com>
2019-03-30 00:51:28 -0500

    configuration bootstrap jquery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavarComponent } from './components/shared/navar/navar.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavarComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
