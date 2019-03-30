694e1c5c1d02169b3551f72db501ea39753c55f0
Ediel96 <h.amiltone.diel@gmail.com>
2019-03-30 10:07:51 -0500

    adding routerLink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//rutas
+import {APP_ROUTING} from './app.routes'
+
 import { AppComponent } from './app.component';
 import { NavarComponent } from './components/shared/navar/navar.component';
 import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
+import { EventComponent } from './components/event/event.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavarComponent,
     AboutComponent,
-    HomeComponent
+    HomeComponent,
+    EventComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
