d8738e1d5cd4df0ac1174c03f3faad33bdf69d2c
Ediel96 <h.amiltone.diel@gmail.com>
2019-03-30 01:08:46 -0500

    Adding home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavarComponent } from './components/shared/navar/navar.component';
 import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavarComponent,
-    AboutComponent
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
